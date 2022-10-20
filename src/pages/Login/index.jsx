8879bbba7c62631d5fb2250156d5230e228dc10a
Aleksandr <it.kucherov@gmail.com>
2022-10-20 12:45:51 +0300

    функционал для авторизации

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,28 +1,65 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import { useForm } from 'react-hook-form';
 
 import styles from './Login.module.scss';
+import { fetchAuth } from '../../redux/slices/auth';
 
 export const Login = () => {
+	const dispatch = useDispatch();
+
+	//используем хук useForm для работы с формой
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: { errors, isValid },
+	} = useForm({
+		defaultValues: {
+			email: 'test@test.ru',
+			password: '12345',
+		},
+		//валидация должна происходить когда поменяеются поля
+		mode: 'onChange',
+	});
+
+	const onSubmit = (values) => {
+		dispatch(fetchAuth(values));
+	};
+
 	return (
 		<Paper classes={{ root: styles.root }}>
 			<Typography classes={{ root: styles.title }} variant="h5">
 				Вход в аккаунт
 			</Typography>
-			<TextField
-				className={styles.field}
-				label="E-Mail"
-				error
-				helperText="Неверно указана почта"
-				fullWidth
-			/>
-			<TextField className={styles.field} label="Пароль" fullWidth />
-			<Button size="large" variant="contained" fullWidth>
-				Войти
-			</Button>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<TextField
+					className={styles.field}
+					label="E-Mail"
+					type="email"
+					error={Boolean(errors.email?.message)}
+					helperText={errors.email?.message}
+					//регистрируем поле
+					{...register('email', { required: 'Укажите почту' })}
+					fullWidth
+				/>
+				<TextField
+					className={styles.field}
+					label="Пароль"
+					error={Boolean(errors.password?.message)}
+					helperText={errors.password?.message}
+					//регистрируем поле
+					{...register('password', { required: 'Введите пароль' })}
+					fullWidth
+				/>
+				<Button type="submit" size="large" variant="contained" fullWidth>
+					Войти
+				</Button>
+			</form>
 		</Paper>
 	);
 };
